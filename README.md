e5df9584de238b4a2c1ad0856ce53353f93af7e4
Nathan James <nathanjms@outlook.com>
2023-01-27 13:36:47 +0000

    Typescript

---
+++ README.md
@@ -0,0 +1,14 @@
+# CLI Env Toggler
+
+Idea: Finds any ENV variables beginning with '###' and enables them, inverting any that already exist to have '###' at the front.
+
+## Dev
+
+```
+npm run build && npm i -g
+```
+
+Then can do
+```
+npx env-toggler
+```
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
-import inquirer from "inquirer";
 import os from "os";
 import fs from "fs/promises";
 
-const userHomeDir = os.homedir();
 const currentDir = process.cwd();
 
 async function main() {
   let envFileContents = await getEnvFile();
-  console.log(envFileContents.split("\n"));
+  console.log(envFileContents.match(/^###.*/gm));
 }
 
 async function getEnvFile(name = ".env") {
